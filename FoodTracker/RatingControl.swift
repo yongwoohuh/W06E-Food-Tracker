1e2685f26d06a00949043dc50c3af6b6444e9a76
yongwoohuh <yongwoo.huh@me.com>
2018-02-24 17:30:35 -0500

    add 5 red buttons for rating

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class RatingControl: UIStackView {
-
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
     //MARK: Initialization
     override init(frame: CGRect) {
-     super.init(frame: frame)
+        super.init(frame: frame)
         setupButtons()
     }
     
@@ -21,18 +25,32 @@ class RatingControl: UIStackView {
         setupButtons()
     }
     
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
     //MARK: Private Methods
     private func setupButtons() {
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
         
-        // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        // Add the button to the stack
-        addArrangedSubview(button)
+        for _ in 0 ..< 5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new buttons to the rating button array
+            ratingButtons.append(button)
+        }
     }
 }
